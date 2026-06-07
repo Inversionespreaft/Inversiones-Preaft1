7ebf2bf0a786566f0da81a06bfcd1f88c1c3cc8f
Elias Rosales <eliasx8r@gmail.com>
2026-06-07 17:52:12 -0500

    arreglo general tienda

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const idCatalogoActual = 7;
 const slugActual = "InversionesPreaft";
 const numeroWhatsApp = "986820428";
-
+const nombreCatalogo = "InversionesPreaft";
 const cart = [];
 let currentProduct = null;
 let selectedOption = null;
